740b700edc49225131cc2805c7651846e81c72f1
Dzony96 <32269577+Dzony96@users.noreply.github.com>
2017-10-09 16:41:43 +0200

    cw3

---
+++ cw3/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+struct Probka{
+double t
+double x
+Probka(double_t, double_x)
+    {t=_t;
+    x=_x;}
+};
+
+vector <Probka> wczytaj (string)
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+throw runtime_error(sygnal.csv);
+}
+string nazwa_pliku = argv[1];
+cout << ”Odczytuje plik: ” << nazwa_pliku << endl;
+}
